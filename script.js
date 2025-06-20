5e5b5ae856f23182667992b73bf2644da3052239
L01272 <baldwinkody1@gmail.com>
2025-06-20 14:50:58 -0500

    Add playRound logic

--- script.js
+++ script.js
@@ -1,37 +1,83 @@
-//computer opponent logic
+let humanScore = 0;
+let computerScore = 0;
+let computerChoice = '';
+let humanChoice = '';
+
 
 function getComputerChoice() {
     let randomNumber = Math.random();
-    let computerChoice = '';
 
     if (randomNumber <= 0.33) {
-        computerChoice = 'Rock'
+        computerChoice = 'rock';
     } else if (randomNumber >= 0.34 && randomNumber <= 0.66) {
-        computerChoice = 'Paper'
+        computerChoice = 'paper';
     } else {
-        computerChoice = 'Scissors'
+        computerChoice = 'scissors';
     }
     console.log('Computer has selected: ' + computerChoice)
 }
 
-//human logic
+//Assume user will always input a valid choice to save time.
+function getHumanChoice() {
+    humanChoice = prompt('Please choose: Rock, Paper, Scissors').toLowerCase();
+    console.log('Player has selected: ' + humanChoice)
+}
 
-//getHumanChoice, use prompt(). Assume user will always input a valid choice to save time.
+function tellScore() {
+    console.log("User score: " + humanScore)
+    console.log("Computer score: " + computerScore)
+}
 
-//return what user selected with console.log
+function playRound(computerChoice, humanChoice) {
+    let humanWins;
+    console.log(humanChoice);
+    console.log(computerChoice);
 
-//scoring
+    if (computerChoice == humanChoice) {
+        console.log("It's a tie! " + computerChoice + " is equal to " + humanChoice)
+        tellScore();
+        return;
+    }
 
-//function humanScore, init with 0, ++ with every win
+    if (humanChoice == 'rock') {
+        if (computerChoice == 'scissors') {
+            humanWins = true;
+        } else {
+            humanWins = false;
+        }
 
-//function computerScore, init with 0, ++ with every win
+    } else if (humanChoice == 'paper') {
+        if (computerChoice == 'rock') {
+            humanWins = true;
+        } else {
+            humanWins = false;
+        }
+
+    } else if (humanChoice == 'scissors') {
+        if (computerChoice == 'paper') {
+            humanWins = true;
+        } else {
+            humanWins = false;
+        }
+
+    }
+
+    if (humanWins) {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+        tellScore();
+    } else {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+        tellScore();
+    }
+}
+
+getComputerChoice();
+getHumanChoice();
+playRound(computerChoice, humanChoice);
 
 //game logic
-
-//function playRound will take two parameters, humanChoice and computerChoice
-//make player choice insensitve to uppercase and lowercase (just make this all lowercase)
-//go through an if block to determine who wins. If human chooses paper and computer choose scissors, print lose and ++ computer score
-//at the end of playRound, console.log something like "You lose! Paper beats Rock". Combine the choices above to print this string
 
 //new function playGame that calls playRound five times
 //play to 5 rounds (index somewhere out of playRound that counts up to 5)
